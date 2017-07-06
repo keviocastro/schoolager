56559470e132e983b311559a76db02ce9af7a2f5
Kévio Castro <keviocastro@gmail.com>
2017-07-06 10:42:10 -0300

    Set environment  variables for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  host: "http://localhost:4200/auth",
-  schools_api_uri: "http://schools-dev.logoseducacao.com.br/",
+  host: 'http://gestor.logosedtech.com.br/auth',
+  schools_api_uri: 'http://schools-dev.logoseducacao.com.br/',
   auth0: {
-    client_id: "XxjI1CTNTGYN9tbBx0yJmtMvvTbt5hWW",
-    domain: "logos.auth0.com",
-    logo_uri: "./assets/img/logos-azul.png",
-    redirect_uri: "http://localhost:4200/auth",
+    client_id: 'XxjI1CTNTGYN9tbBx0yJmtMvvTbt5hWW',
+    domain: 'logos.auth0.com',
+    logo_uri: './assets/img/logos-azul.png',
+    redirect_uri: 'http://gestor.logosedtech.com.br/auth',
     audience: 'api.schools',
     scope : 'openid profile email'
   },
